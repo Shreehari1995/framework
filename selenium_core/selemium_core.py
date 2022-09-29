62f9c30d646d85e75a47689bb1076f783720fd2f
krishnavams01~ <vamsi.krishna766@gmail.com>
2022-09-29 15:18:11 +0530

    scvbshjdgcq

--- selenium_core/selemium_core.py
+++ selenium_core/selemium_core.py
@@ -1,15 +1,11 @@
 import pytest
-from selenuium.webdriver import ActionChains
+from selenium.webdriver import ActionChains
 
 
 @pytest.mark.usefixtures("setup")
 class SeleniumCore:
     def __init__(self, setup):
         self.driver = setup
-
-    #  asdfghjkl;cvbnm,
-    #  asdfghjkl;cvbnm,
-    #  asdfghjkl;cvbnm,
 
     def get_element(self, loc_type, loc_value):
         return self.driver.find_element(loc_type, loc_value)
@@ -19,30 +15,8 @@ class SeleniumCore:
 
     def send_keys(self, loc_type, loc_value):
         self.get_element(loc_type, loc_value).send_keys()
-        
+
     def move_to_element(self, locator, by_type):
-        AnctionChains(self.driver).move_to_element(self.get_element(locator, by_type))
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
+        ActionChains(self.driver).move_to_element(self.get_element(locator, by_type))
+
+    #   kefghegfjergyugfhjehry
